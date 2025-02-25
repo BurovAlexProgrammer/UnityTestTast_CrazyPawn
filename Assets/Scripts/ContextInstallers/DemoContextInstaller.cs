2948a8ad865a9bb33ed44bc0198c48137605ffc1
Aleksey Burov <burovalexprogrammer@gmail.com>
2025-02-25 06:58:42 +0200

    Board generator

---
+++ Assets/Scripts/ContextInstallers/DemoContextInstaller.cs
@@ -0,0 +1,18 @@
+﻿using CrazyPawn;
+using Services;
+using UnityEngine;
+using Zenject;
+
+namespace ContextInstallers
+{
+    public class DemoContextInstaller : MonoInstaller
+    {
+        [SerializeField] private CrazyPawnSettings _crazyPawnSettings;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<CrazyPawnSettings>().FromInstance(_crazyPawnSettings).AsSingle();
+            Container.Bind<BoardGenerator>().FromNew().AsSingle();
+        }
+    }
+}
---
+++ Assets/Scripts/EntryPoint/DemoEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Services;
+using UnityEngine;
+using Zenject;
+
+namespace EntryPoint
+{
+    public class DemoEntryPoint : MonoBehaviour
+    {
+        [Inject] private BoardGenerator _boardGenerator;
+
+        private void Awake()
+        {
+            _boardGenerator.Generate(Vector3.zero);
+        }
+    }
+}
